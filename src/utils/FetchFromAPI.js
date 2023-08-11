adfe4f3b14008efc127cfe626793ac70b17bc1f2
Jatin Hindujha <jatinhinduja4@gmail.com>
2023-08-11 12:24:14 +0530

    nn

--- src/utils/FetchFromAPI.js
+++ src/utils/FetchFromAPI.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-
+  url: `${BASE_URL}`,
   params: {
     maxResults: '50',
   },
   headers: {
-    'X-RapidAPI-Key': 'fb7301bb20msh2efddc7457713d3p158efcjsnd95eb5e1f06c',
+    'X-RapidAPI-Key': `${process.env.REACT_APP_RAPID_API_KEY}`,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
